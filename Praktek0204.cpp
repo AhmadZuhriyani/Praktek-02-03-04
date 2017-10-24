02960da8901fc3a3b52fb712621a4b38b2e060fa
AhmadZuhriyani <izuhryan@gmail.com>
2017-10-24 12:09:18 +0800

    Menambahkan Cout
    15

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -4,4 +4,8 @@ int main(){
 	double luasRumah = 42;
 	double lantaiP = 193*1215;
 	double luas1Box = lantaiP*8/1000000;
+	
+	cout << "Luas Rumah		: "<<luasRumah<<" m2"<<endl;
+	cout << "Luas 1 Box Parket	: "<<luas1Box<<" m2"<<endl;
 }
+
